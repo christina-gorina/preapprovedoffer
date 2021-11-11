732dbc9dee3ba7160dfa1089489d1095e13e98fe
christina-gorina <christina.gorina@yandex.ru>
2021-11-11 20:01:40 +0300

    Создание и заполнение черных списков

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -35,3 +35,24 @@ values (1002, 1002, 'silver', false);
 insert into client_address (client_id, address_id)
 values (1002, 1001);
 
+insert into passport_black_list (id, number)
+values (1004, '1498914545');
+values (1005, '1216185749');
+values (1006, '1866858709');
+values (1007, '1616838746');
+values (1008, '1425178756');
+
+insert into phone_black_list (id, number)
+values (1009, '+79161352739');
+values (1010, '+79167975633');
+values (1011, '+79186758638');
+values (1012, '+79129866734');
+values (1013, '+79141396730');
+
+insert into address_black_list (id, name)
+values (1014, '117544, Россия, г.Тула, ул.Мира, д.16, к.1, кв.50');
+values (1015, '118534, Россия, г.Москва, ул.Тверская, д.19, к.3, кв.9');
+values (1016, '147661, Россия, г.Москва, ул.Белая, д.56, к.1, кв.52');
+values (1017, '118534, Россия, г.Москва, ул.Правды, д.89, к.4, кв.76');
+values (1018, '116566, Россия, г.Москва, ул.Озерная, д.48, к.1, кв.69');
+
